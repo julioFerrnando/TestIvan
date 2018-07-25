86ba1554764ecfc78edea5a4330146d96883695b
JulioOT <JulioOT>
2018-07-25 16:02:26 -0600

    create carpet

---
+++ CppApplication_2/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Julio Fernando Ixcoy 
+ *
+ * Created on 25 de julio de 2018, 04:01 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
